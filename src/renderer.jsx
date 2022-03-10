60b667af1336431e84c75265b5b400576f14fd04
Will <wbrubaker@gmail.com>
2022-03-09 16:51:13 -1000

    insert the stylesheet

--- src/renderer.jsx
+++ src/renderer.jsx
@@ -8,6 +8,10 @@ const addonID = packageJSON['slug'];
 export default function ( context ) {
 	const {React, hooks} = context;
 	const {Route} = context.ReactRouter;
+	const stylesheetPath = path.resolve(__dirname, '../style.css');
+
+	//insert our stylesheet
+	hooks.addContent('stylesheets', () => <link rel="stylesheet" key="notes-addon-styleesheet" href={stylesheetPath} />);
 
 	// Create the route/page of content that will be displayed when the menu option is clicked
 	hooks.addContent( 'routesSiteInfo', () =>
