3d2e42997a62586fba71fdddc0398b92ef222c78
Will <wbrubaker@gmail.com>
2022-03-14 16:24:42 -1000

    fewer functions.

--- src/Addon.jsx
+++ src/Addon.jsx
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
-import { ipcRenderer } from 'electron';
-const { exec } = require('child_process');
+import React, { Component } from "react";
+import { ipcRenderer } from "electron";
+const { exec } = require("child_process");
 
 // https://getflywheel.github.io/local-addon-api/modules/_local_renderer_.html
-import * as LocalRenderer from '@getflywheel/local/renderer';
+import * as LocalRenderer from "@getflywheel/local/renderer";
 
 // https://github.com/getflywheel/local-components
-import { Button, FlyModal, Title, Text, Tooltip } from '@getflywheel/local-components';
+import {
+	Button,
+	FlyModal,
+	Title,
+	Text,
+	Tooltip,
+} from "@getflywheel/local-components";
 
 export default class Boilerplate extends Component {
 	constructor(props) {
@@ -15,28 +21,32 @@ export default class Boilerplate extends Component {
 		this.state = {
 			siteId: props.match.params.siteID,
 			showInstructions: false,
-			localeSwitchedTo: '',
+			showError: false,
+			localeSwitchedTo: "",
 		};
 
 		this.hideInstructions = this.hideInstructions.bind(this);
-		this.switchToEuro = this.switchToEuro.bind(this);
-		this.switchToUS = this.switchToUS.bind(this);
-		this.switchToAus = this.switchToAus.bind(this);
-		this.switchToCanada = this.switchToCanada.bind(this);
+		this.hideError = this.hideError.bind(this);
 		this.testRequest = this.testRequest.bind(this);
 		this.launchPostman = this.launchPostman.bind(this);
 	}
 
 	componentDidMount() {
-		ipcRenderer.on('instructions', (event) => {
+		ipcRenderer.on("instructions", (event) => {
 			this.setState({
 				showInstructions: true,
+			});
+		});
+
+		ipcRenderer.on("error", (event) => {
+			this.setState({
+				showError: true,
 			});
 		});
 	}
 
 	componentWillUnmount() {
-		ipcRenderer.removeAllListeners('instructions');
+		ipcRenderer.removeAllListeners("instructions");
 	}
 
 	hideInstructions() {
@@ -45,92 +55,23 @@ export default class Boilerplate extends Component {
 		});
 	}
 
-	switchToEuro() {
-		this.localeSwitchedTo = 'Europe';
-		ipcRenderer.send(
-			'switch-country',
-			this.state.siteId,
-			{ 
-				'woocommerce_store_address': 'Brederopad 77',
-				'woocommerce_store_address_2': '',
-				'woocommerce_store_city': 'Delft',
-				'woocommerce_default_country': 'NL',
-				'woocommerce_store_postcode': '2624 XR',
-				'woocommerce_currency': 'EUR',
-				'woocommerce_price_thousand_sep': ' ',
-				'woocommerce_price_decimal_sep' : ',',
-				'woocommerce_weight_unit': 'kg',
-				'woocommerce_dimension_unit': 'cm',
-			}
-		);
+	hideError() {
+		this.setState({
+			showError: false,
+		});
 	}
-	
-	switchToAus() {
-		this.localeSwitchedTo = 'Australia';
-		ipcRenderer.send(
-			'switch-country',
-			this.state.siteId,
-			{ 
-				'woocommerce_store_address': '28 Kaesler Road',
-				'woocommerce_store_address_2': '',
-				'woocommerce_store_city': 'Mount Burr',
-				'woocommerce_default_country': 'AU:SA',
-				'woocommerce_store_postcode': '5279',
-				'woocommerce_currency': 'AUD',
-				'woocommerce_price_thousand_sep': ' ',
-				'woocommerce_price_decimal_sep' : ',',
-				'woocommerce_weight_unit': 'kg',
-				'woocommerce_dimension_unit': 'cm',
-			}
-		);
-	}	
-	
-	switchToCanada() {
-		this.localeSwitchedTo = 'Canada';
-		ipcRenderer.send(
-			'switch-country',
-			this.state.siteId,
-			{ 
-				'woocommerce_store_address': '40 Bay St',
-				'woocommerce_store_address_2': '',
-				'woocommerce_store_city': 'Toronto',
-				'woocommerce_default_country': 'CA:ON',
-				'woocommerce_store_postcode': 'M5J 2X2',
-				'woocommerce_currency': 'CAD',
-				'woocommerce_price_thousand_sep': ' ',
-				'woocommerce_price_decimal_sep' : ',',
-				'woocommerce_weight_unit': 'kg',
-				'woocommerce_dimension_unit': 'cm',
-			}
-		);
-	}	
-	
-	switchToUS() {
-		this.localeSwitchedTo = 'United States';
-		ipcRenderer.send(
-			'switch-country',
-			this.state.siteId,
-			{ 
-				'woocommerce_store_address': '537 Paper Street',
-				'woocommerce_store_address_2': '#34',
-				'woocommerce_store_city': 'Wilmington',
-				'woocommerce_default_country': 'US:DE',
-				'woocommerce_store_postcode': '19806',
-				'woocommerce_currency': 'USD',
-				'woocommerce_price_thousand_sep': ',',
-				'woocommerce_price_decimal_sep' : '.',
-				'woocommerce_weight_unit': 'lbs',
-				'woocommerce_dimension_unit': 'in',
-			}
-		);
+
+	switchCountry(newLocale, optionsToSet) {
+		ipcRenderer.send("switch-country", this.state.siteId, optionsToSet);
+		this.localeSwitchedTo = newLocale;
 	}
 
 	testRequest() {
-		ipcRenderer.send('test-request');
+		ipcRenderer.send("test-request");
 	}
 
 	launchPostman() {
-		exec('open -a Postman');
+		exec("open -a Postman");
 	}
 
 	renderInstructions() {
@@ -139,37 +80,162 @@ export default class Boilerplate extends Component {
 				isOpen={this.state.showInstructions}
 				onRequestClose={this.hideInstructions}
 			>
-				<Title fontSize='xl'>Great Success!</Title>
-				<div style={{padding: '20px'}}>
+				<Title fontSize="xl">Great Success!</Title>
+				<div style={{ padding: "20px" }}>
 					<Text
-						fontSize='l'
+						fontSize="l"
 						privateOptions={{
-							fontWeight: 'medium',
+							fontWeight: "medium",
 						}}
 					>
-						Site locale switcheroo to {this.localeSwitchedTo} happened without incident!
+						Site locale switcheroo to {this.localeSwitchedTo}{" "}
+						happened without incident!
 					</Text>
 				</div>
 			</FlyModal>
-		)
+		);
 	}
 
-    render() {
-        return (
-            <div style={{ flex: '1', overflowY: 'auto', margin: '10px' }}>
+	renderError() {
+		return (
+			<FlyModal
+				isOpen={this.state.showError}
+				onRequestClose={this.hideError}
+			>
+				<Title fontSize="xl">Much Sadness :(</Title>
+				<div style={{ padding: "20px" }}>
+					<Text
+						fontSize="l"
+						privateOptions={{
+							fontWeight: "medium",
+						}}
+					>
+						There was some sort of an error. Check the logs maybe.
+					</Text>
+				</div>
+			</FlyModal>
+		);
+	}
+
+	render() {
+		return (
+			<div style={{ flex: "1", overflowY: "auto", margin: "10px" }}>
 				{this.renderInstructions()}
+				{this.renderError()}
 				<div>
-					<ul style={{ listStyle: 'none' }}>
-					<li><Button onClick={this.switchToUS} className="woo button">Switch Site to US</Button></li>
-					<li><Button onClick={this.switchToEuro} className="woo button">Switch Site to Europe</Button></li>
-					<li><Button onClick={this.switchToAus} className="woo button">Switch Site to Australia</Button></li>
-					<li><Button onClick={this.switchToCanada} className="woo button">Switch Site to Canada</Button></li>
-					<li><Button onClick={this.testRequest} className="woo button">Test an HTTP request</Button></li>
-					<li><Button onClick={this.launchPostman} className="woo button">Launch Postman</Button></li>
+					<ul style={{ listStyle: "none" }}>
+						<li>
+							<Button
+								onClick={this.switchCountry.bind(
+									this,
+									"United States",
+									{
+										woocommerce_store_address: "537 Paper Street",
+										woocommerce_store_address_2: "#34",
+										woocommerce_store_city: "Wilmington",
+										woocommerce_default_country: "US:DE",
+										woocommerce_store_postcode: "19806",
+										woocommerce_currency: "USD",
+										woocommerce_price_thousand_sep: ",",
+										woocommerce_price_decimal_sep: ".",
+										woocommerce_weight_unit: "lbs",
+										woocommerce_dimension_unit: "in",
+									}
+								)}
+								className="woo button"
+							>
+								Switch Site to United States
+							</Button>
+						</li>
+						<li>
+							<Button
+								onClick={this.switchCountry.bind(
+									this,
+									"Europe",
+									{
+										woocommerce_store_address: "Brederopad 77",
+										woocommerce_store_address_2: "",
+										woocommerce_store_city: "Delft",
+										woocommerce_default_country: "NL",
+										woocommerce_store_postcode: "2624 XR",
+										woocommerce_currency: "EUR",
+										woocommerce_price_thousand_sep: " ",
+										woocommerce_price_decimal_sep: ",",
+										woocommerce_weight_unit: "kg",
+										woocommerce_dimension_unit: "cm",
+									}
+								)}
+								className="woo button"
+							>
+								Switch Site to Europe
+							</Button>
+						</li>
+						<li>
+							<Button
+								onClick={this.switchCountry.bind(
+									this,
+									"Australia",
+									{
+										woocommerce_store_address: "28 Kaesler Road",
+										woocommerce_store_address_2: "",
+										woocommerce_store_city: "Mount Burr",
+										woocommerce_default_country: "AU:SA",
+										woocommerce_store_postcode: "5279",
+										woocommerce_currency: "AUD",
+										woocommerce_price_thousand_sep: " ",
+										woocommerce_price_decimal_sep: ",",
+										woocommerce_weight_unit: "kg",
+										woocommerce_dimension_unit: "cm",
+										woocommerce_dimension_unit: "in",
+									}
+								)}
+								className="woo button"
+							>
+								Switch Site to Australia
+							</Button>
+						</li>
+						<li>
+							<Button
+								onClick={this.switchCountry.bind(
+									this,
+									"Canada",
+									{
+										woocommerce_store_address: "40 Bay St",
+										woocommerce_store_address_2: "",
+										woocommerce_store_city: "Toronto",
+										woocommerce_default_country: "CA:ON",
+										woocommerce_store_postcode: "M5J 2X2",
+										woocommerce_currency: "CAD",
+										woocommerce_price_thousand_sep: " ",
+										woocommerce_price_decimal_sep: ",",
+										woocommerce_weight_unit: "kg",
+										woocommerce_dimension_unit: "cm",
+									}
+								)}
+								className="woo button"
+							>
+								Switch Site to Canada
+							</Button>
+						</li>
+						<li>
+							<Button
+								onClick={this.testRequest}
+								className="woo button"
+							>
+								Test an HTTP request
+							</Button>
+						</li>
+						<li>
+							<Button
+								onClick={this.launchPostman}
+								className="woo button"
+							>
+								Launch Postman
+							</Button>
+						</li>
 					</ul>
 				</div>
-            </div>
-        )
-    }
-
+			</div>
+		);
+	}
 }
